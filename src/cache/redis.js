230c6ca711afc5ecaa2d871bced1aaed3cf90450
Sarmiento Mr. D. (Dalessandro) <dsarmiento@arellano.pe>
2025-03-24 12:01:40 -0500

    package

---
+++ src/cache/redis.js
@@ -0,0 +1,11 @@
+// src/cache/redis.js
+import Redis from 'ioredis';
+
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+  tls: {} // Opcional si tu Redis usa SSL interno
+});
+
+export default redis;
