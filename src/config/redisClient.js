2d28f8a70b8f5c747c33b9917c5054137cb715d7
Sarmiento Mr. D. (Dalessandro) <dsarmiento@arellano.pe>
2025-03-25 10:38:02 -0500

    redis client

--- src/config/redisClient.js
+++ src/config/redisClient.js
@@ -1,11 +1,12 @@
 import { createClient } from 'redis';
 
 const redisClient = createClient({
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
   socket: {
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
-  },
-  password: process.env.REDIS_PASSWORD,
+  }
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error:', err));
