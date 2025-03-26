67559bf1a48e80b0c3cc8283ab53096f2813edb5
Sarmiento Mr. D. (Dalessandro) <dsarmiento@arellano.pe>
2025-03-26 16:05:50 -0500

    proyecciones

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
 import metricasRoutes from "./routes/metricasRoutes.js";
+import proyeccionesRoutes from "./routes/proyeccionesRoutes.js";
 import { connectRedis } from './config/redisClient.js';
 
 dotenv.config();
@@ -21,6 +22,7 @@ app.use(express.json());
 // Rutas
 app.use('/api/auth', authRoutes);
 app.use("/api/metricas", metricasRoutes);
+app.use('/api', proyeccionesRoutes);
 
 // Server
 const PORT = process.env.PORT || 4000;
