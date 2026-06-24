1790da96e9bdaaa9dcf1cf5f9fd9f74e62ecabfa
Piero010405 <psarmientoayala@gmail.com>
2026-06-23 23:18:42 -0500

    up(metricsController)

--- src/controllers/metricasController.js
+++ src/controllers/metricasController.js
@@ -3,8 +3,13 @@ import { obtenerMetricas } from "../services/metricasService.js";
 export const metricasController = async (req, res) => {
   try {
     const metricas = await obtenerMetricas(req.user);
-    
-    const { notaudited, cancelled, fueraDT, fullAudit, tiendasFaltantes } = metricas;
+
+    const notaudited = Number(metricas?.notaudited ?? 0);
+    const cancelled = Number(metricas?.cancelled ?? 0);
+    const fueraDT = Number(metricas?.fueraDT ?? 0);
+    const fullAudit = Number(metricas?.fullAudit ?? 0);
+    const tiendasFaltantes = Number(metricas?.tiendasFaltantes ?? 0);
+
     const totalTiendas = notaudited + cancelled + fueraDT;
 
     return res.json({
